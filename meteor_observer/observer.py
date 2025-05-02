adeacf8b15e80fe61d4b20a48bc322100c5b9fee
s240506r <s246506r@st.go.tuat.ac.jp>
2025-05-02 13:17:35 +0900

    流星検知プログラム追加

---
+++ meteor_observer/observer.py
@@ -0,0 +1,305 @@
+import glob
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import scipy
+from scipy import interpolate
+import math
+import time
+import os
+from threading import Lock
+lock = Lock()
+import shutil
+import threading
+import datetime
+import socket
+
+ip = "127.0.0.1"
+port = 8000
+
+class Process_image():
+    
+    def __init__(self):
+        self.min_length = 10
+        self.save_path = ""
+        
+    def detect_line(self, img, min_length):
+        """画像上の線状のパターンを流星として検出する。
+        Args:
+        img: 検出対象となる画像
+        min_length: HoughLinesPで検出する最短長(ピクセル)
+        Returns:
+        検出結果
+        """
+        blur_size = (5, 5)
+        blur = cv2.GaussianBlur(img, blur_size, 0)
+        canny = cv2.Canny(blur, 100, 200, 3)
+
+        # The Hough-transform algo:
+        return cv2.HoughLinesP(canny, 1, np.pi/180, 25, minLineLength=min_length, maxLineGap=5)
+
+
+    def diff(self, img_list, mask=None):
+        """画像リストから差分画像のリストを作成する。
+
+        Args:
+        img_list: 画像データのリスト
+        mask: マスク画像(2値画像)
+
+        Returns:
+        差分画像のリスト
+        """
+        diff_list = []
+        if mask is None:
+            mask = np.zeros(img_list[0].shape, dtype=np.uint8)
+
+        for img1, img2 in zip(img_list[:-2], img_list[1:]):
+            if mask is not None:
+                img1 = cv2.bitwise_or(img1, mask)
+                img2 = cv2.bitwise_or(img2, mask)
+            diff_list.append(cv2.subtract(img1, img2))
+
+        return diff_list
+
+    def brightest(self, img_list):
+        """比較明合成処理
+        Args:
+        img_list: 画像データのリスト
+
+        Returns:
+        比較明合成された画像
+        """
+        output = img_list[0]
+
+        for img in img_list[1:]:
+            output = cv2.max(img, output)
+
+        return output
+
+
+    def detect_meteor(self, img_list):
+        """img_listで与えられた画像のリストから流星(移動天体)を検出する。
+        """
+
+        if len(img_list) > 2:
+            # 差分間で比較明合成を取るために最低3フレームが必要。
+            # 画像のコンポジット(単純スタック)
+            diff_img = self.brightest(self.diff(img_list, None))
+            detected = self.detect_line(diff_img, self.min_length)
+            if detected is not None:
+                '''
+                for meteor_candidate in detected:
+                    print('{} {} A possible meteor was detected.'.format(obs_time, meteor_candidate))
+                '''
+                print("A possible meteor was detected.")
+                #cv2.imwrite(path_name, self.composite_img)
+
+                """
+                # 検出した動画を保存する。
+                movie_file = str(
+                    Path(self.save_path, "movie-" + filename + ".mp4"))
+                self.save_movie(img_list, movie_file)
+                """
+                return True, diff_img
+            else:
+                return False, diff_img
+                
+    def save_movie(self, img_list, pathname):
+        #画像リストから動画を作成する。
+
+        size = (self.WIDTH, self.HEIGHT)
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
+        video = cv2.VideoWriter(pathname, fourcc, self.FPS, size)
+        for img in img_list:
+            video.write(img)
+        video.release()
+            
+class cameras():
+    def __init__(self, path, num):
+        self.image_processer = Process_image()
+        self.files_to_be_del = []#削除する（流星が移っていないファイル）
+        self.files_to_be_saved = []#保存するファイル（写ってるやつ）
+        self.folders_to_be_saved = [] #各写真を保存するフォルダ名を入れる（時刻）
+        self.folder_path=path
+        self.num=num
+        
+        self.regular_record_interval = 60*60*0 + 60*15 + 0 #定時記録の間隔を秒で指定
+        
+        #定時記録を保存するパス
+        self.regular_record_path = self.folder_path + "/regular_record"
+        try:
+            os.mkdir(self.regular_record_path)
+        except:
+            pass
+    def read_imgs(self, folder_path):
+        files=glob.glob(folder_path)
+        return files
+    
+
+    
+    def organize_files(self):
+        while True:
+            time.sleep(0.5)
+            #print(self.files_to_be_del)
+            if len(self.files_to_be_del)!=0:
+                self.delete_pictures()
+            if len(self.files_to_be_saved)!=0:
+                self.save_detected_result()
+
+    def delete_pictures(self):
+        with lock:
+            ftpd = self.files_to_be_del.copy()
+            self.files_to_be_del = []
+        for path in ftpd:
+            os.remove(path)
+        
+    def save_detected_result(self):
+        with lock:
+            ftbs = self.files_to_be_saved.copy()
+            fotbs = self.folders_to_be_saved.copy()
+            self.files_to_be_saved = []
+            self.folders_to_be_saved = []
+        for i in range(len(ftbs)): 
+            try:   
+                os.mkdir(fotbs[i])
+            except:
+                pass
+            for file in ftbs[i]:
+                try:
+                    shutil.move(file, fotbs[i])
+                except:
+                    pass
+            
+            
+            
+    #撮影済みデータから見つけるとき
+    def folder_camera(self, folder_path, num):
+        
+        files = read_imgs(folder_path + "/*png*")
+        img = cv2.imread(files[0])
+        img_base = np.zeros((img.shape[0],img.shape[1],3))
+
+        img_list=[]
+        for i in range(len(files)):
+            img = cv2.imread(files[i])
+            img_list.append(img)
+            #所定枚数毎に検知をする
+            
+            if i!=0 and i%num==0:
+                self.image_processer.detect_meteor()
+                img_list = []
+    
+    def read_file_index(self, filename):
+        file_index = int((filename.split("_")[-1]).replace(".png", ""))
+        return file_index
+
+    def read_file_base(self, filename):
+        file_base = filename.split("_")[0]
+        return file_base
+
+    def sharpcap_live(self):
+        #sharpcapがフォルダに保存した画像をリアルタイムに取得して処理する関数
+        folder_path=self.folder_path
+        
+        num=self.num
+        img_list=[]
+        last_processed_picture_index = self.read_file_index(self.read_imgs(folder_path + "/*png*")[0]) #最後に検知処理された画像のindex
+        print(last_processed_picture_index)
+        last_regular_record_time = datetime.datetime.now() #最後に経過を記録した時間
+        regular_recorded_file=""#削除予定ファイル名に追加しないようにするために必要
+        
+        while(1):
+            files = self.read_imgs(folder_path + "/*png*")
+            
+            latest_picture_index = self.read_file_index(files[-1])
+            file_base = self.read_file_base(files[-1]) #写真ファイル名のインデックスの前の部分
+
+            img_list = []
+            read_files = [] #読み込むファイル
+
+            dt_now = datetime.datetime.now() #現在時刻取得
+
+
+
+            #検知に必要な枚数がたまったら処理開始
+            if (latest_picture_index - last_processed_picture_index) >num+1:
+                #print(latest_picture_index, last_processed_picture_index, latest_picture_index - last_processed_picture_index )
+                for i in range(num):
+                    filename = folder_path+"/tes_"+str(last_processed_picture_index+i)+".png"
+                    #print(filename)
+                    read_files.append(filename)
+                    img = cv2.imread(filename)
+                    im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    img_list.append(im_gray)
+
+                result, diff_img = self.image_processer.detect_meteor(img_list)
+                last_processed_picture_index +=num             
+                
+                cv2.imshow("", diff_img )
+                cv2.waitKey(10)
+                
+                img_list.clear()
+                #print(latest_picture_index, last_processed_picture_index, latest_picture_index - last_processed_picture_index )
+                
+                if result == True:#検知した時
+                    with lock:
+                        self.files_to_be_saved.append(read_files)
+                        self.folders_to_be_saved.append(self.folder_path + "/"+ dt_now.strftime('%d-%H_%M_%S'))
+                #定時記録
+                if (dt_now - last_regular_record_time).seconds > self.regular_record_interval and len(files)!=0:
+                    with lock:
+                        self.files_to_be_saved.append([files[-1]])
+                        self.folders_to_be_saved.append(self.regular_record_path)
+                    regular_recorded_file=files[-1]
+                    last_regular_record_time = dt_now 
+                                    
+                if result == False:#検知されなかったとき
+                    if regular_recorded_file in read_files:
+                        read_files.remove(regular_recorded_file) #定時記録用写真は削除しないように除く
+                    with lock:
+                        self.files_to_be_del+=read_files #削除予定のファイルに追加
+            
+            
+            time.sleep(0.2)
+
+    #botプロセスと通信する関数。スレッド立てて動かす
+    def socket_client(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+
+        print("Connected!!!!!")
+
+        while True:
+            print("<メッセージを入力してください>")
+            message = input('>>>')
+            if not message:
+                s.send("quit".encode("utf-8"))
+                break
+            s.send(message.encode("utf-8"))
+            receive = s.recv(4096).decode()
+            print(receive + "\n")
+        s.close()
+
+    def process(self):
+        thread1 = threading.Thread(target=self.sharpcap_live)
+        thread2 = threading.Thread(target=self.organize_files)
+        #thread3 = threading.Thread(target=self.socket_client_files)
+        
+        thread1.start()
+        thread2.start()
+        #thread3.start()
+        
+        thread1.join()
+        thread2.join() 
+        #thread3.join()  
+
+def communicate_bot():
+    #botのプロセスと通信する
+    pass
+
+
+path = input("folder path=")
+num = int(input("num for detection"))
+camera = cameras(path, num)
+camera.process()
---
+++ meteor_observer/sock_client_test.py
@@ -0,0 +1,23 @@
+import socket
+
+ip = "127.0.0.1"
+port = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+
+print("Connected!!!!!")
+
+while True:
+	print("<メッセージを入力してください>")
+	message = input('>>>')
+	if not message:
+		s.send("quit".encode("utf-8"))
+		break
+	s.send(message.encode("utf-8"))
+	receive = s.recv(4096).decode()
+	print(receive + "\n")
+	
+s.close()
+
+print("END")
